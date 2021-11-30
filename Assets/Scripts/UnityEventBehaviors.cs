64365b45652220e09ba77b461381192869ad68c4
Sam Stallings <10915865@my.uvu.edu>
2021-11-29 17:27:30 -0700

    unity events

---
+++ Assets/Scripts/UnityEventBehaviors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventBehaviors : MonoBehaviour
+{
+    public UnityEventBehaviors awakeEvent, startEvent, onEnableEvent;
+    public float holdTime;
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+
+    private void Invoke()
+    {
+        throw new NotImplementedException();
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(holdTime);
+        startEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+}
